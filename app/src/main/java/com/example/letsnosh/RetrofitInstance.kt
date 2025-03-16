ea669c6762f095c38497da5ec8e3d29545fadb25
rhsajalLimeroad <sajal.ruhela@limeroad.com>
2025-03-16 20:30:43 +0530

    cleaned dependencies

--- app/src/main/java/com/example/letsnosh/RetrofitInstance.kt
+++ app/src/main/java/com/example/letsnosh/RetrofitInstance.kt
@@ -1,9 +1,6 @@
 package com.example.letsnosh
 
-import kotlinx.serialization.json.Json
 import retrofit2.Retrofit
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
